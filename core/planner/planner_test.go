fc0cab0e962c9641a1e40b567b992cb3291e9096
andig <cpuidle@gmx.de>
2022-12-18 12:38:34 +0100

    Simplof structure

--- core/planner/planner_test.go
+++ core/planner/planner_test.go
@@ -28,7 +28,7 @@ func rates(prices []float64, start time.Time) api.Rates {
 	return res
 }
 
-func TestIsCheapSlotNow(t *testing.T) {
+func TestPlanner(t *testing.T) {
 	dt := time.Hour
 	ctrl := gomock.NewController(t)
 
@@ -54,7 +54,7 @@ func TestIsCheapSlotNow(t *testing.T) {
 			{5*dt - 20*time.Minute, 20 * time.Minute, true},
 			{5*dt + 1, 5 * time.Minute, false}, // after desired charge timer,
 		}},
-		{"raising prices", []float64{1, 2, 3, 4, 5, 6, 7, 8}, 5 * time.Hour, []se{
+		{"rising prices", []float64{1, 2, 3, 4, 5, 6, 7, 8}, 5 * time.Hour, []se{
 			{1*dt - 1, time.Hour, true},
 			{2*dt - 1, 5 * time.Minute, true}, // charging took longer than expected
 			{3*dt - 1, 0, false},
@@ -68,14 +68,14 @@ func TestIsCheapSlotNow(t *testing.T) {
 			{4*dt + 1, 20 * time.Minute, true}, // start as late as possible
 			{4*dt + 40*time.Minute, 20 * time.Minute, true},
 		}},
-		{"don't stop for last slot", []float64{5, 4, 5, 2, 3, 5, 5, 5}, 5 * time.Hour, []se{
+		{"don't pause last slot", []float64{5, 4, 5, 2, 3, 5, 5, 5}, 5 * time.Hour, []se{
 			{1*dt - 1, 70 * time.Minute, false},
 			{2*dt - 1, 70 * time.Minute, false},
 			{3*dt - 1, 70 * time.Minute, false},
 			{4*dt - 1, 20 * time.Minute, true}, // don't pause last slot
 			{4*dt + 1, 20 * time.Minute, true},
 		}},
-		{"delay expensiv middle", []float64{5, 4, 3, 5, 5, 5, 5, 5}, 5 * time.Hour, []se{
+		{"delay expensive middle", []float64{5, 4, 3, 5, 5, 5, 5, 5}, 5 * time.Hour, []se{
 			{1*dt - 1, 70 * time.Minute, false},
 			{1*dt + 1, 70 * time.Minute, false},
 			{2*dt - 1, 61 * time.Minute, true}, // delayed start on expensiv slot
@@ -88,49 +88,6 @@ func TestIsCheapSlotNow(t *testing.T) {
 			{1, 2 * time.Hour, true},
 			{1, 10 * time.Minute, true},
 		}},
-	}
-
-	clck := clock.NewMock()
-
-	for _, tc := range tc {
-		t.Logf("%+v", tc.desc)
-		t.Logf("set: %v", clck.Now())
-
-		trf := mock.NewMockTariff(ctrl)
-		trf.EXPECT().Rates().AnyTimes().Return(rates(tc.prices, clck.Now()), nil)
-
-		p := &Planner{
-			log:    util.NewLogger("foo"),
-			clock:  clck,
-			tariff: trf,
-		}
-
-		start := clck.Now()
-		for idx, se := range tc.series {
-			clck.Set(start.Add(se.delay))
-
-			res, _ := p.Active(se.cDuration, start.Add(tc.end))
-			assert.Equalf(t, se.res, res, "%s case %d: expected %v, got %v", tc.desc, idx+1, se.res, res)
-		}
-	}
-}
-
-func TestIsCheap(t *testing.T) {
-	dt := time.Hour
-	ctrl := gomock.NewController(t)
-
-	type se struct {
-		delay     time.Duration
-		cDuration time.Duration
-		res       bool
-	}
-
-	tc := []struct {
-		desc   string
-		prices []float64
-		end    time.Duration
-		series []se
-	}{
 		{"always expensive", []float64{5, 4, 3, 2, 1, 0, 0, 0}, 5 * time.Hour, []se{
 			{1*dt - 1, time.Minute, false},
 			{2*dt - 1, time.Minute, false},
@@ -143,25 +100,26 @@ func TestIsCheap(t *testing.T) {
 	clck := clock.NewMock()
 
 	for _, tc := range tc {
-		t.Logf("%+v", tc.desc)
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Logf("set: %v", clck.Now())
 
-		trf := mock.NewMockTariff(ctrl)
-		trf.EXPECT().Rates().AnyTimes().Return(rates(tc.prices, clck.Now()), nil)
+			trf := mock.NewMockTariff(ctrl)
+			trf.EXPECT().Rates().AnyTimes().Return(rates(tc.prices, clck.Now()), nil)
 
-		p := &Planner{
-			log:    util.NewLogger("foo"),
-			clock:  clck,
-			tariff: trf,
-		}
+			p := &Planner{
+				log:    util.NewLogger("foo"),
+				clock:  clck,
+				tariff: trf,
+			}
 
-		start := clck.Now()
+			start := clck.Now()
+			for idx, se := range tc.series {
+				clck.Set(start.Add(se.delay))
 
-		for idx, se := range tc.series {
-			clck.Set(start.Add(se.delay))
-
-			res, _ := p.Active(se.cDuration, start.Add(tc.end))
-			assert.Equalf(t, se.res, res, "%s case %d: expected %v, got %v", tc.desc, idx+1, se.res, res)
-		}
+				res, _ := p.Active(se.cDuration, start.Add(tc.end))
+				assert.Equalf(t, se.res, res, "%s case %d: expected %v, got %v", tc.desc, idx+1, se.res, res)
+			}
+		})
 	}
 }
 
