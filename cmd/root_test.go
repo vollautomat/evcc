ff165cd6cbaaf02e7d59fb5edc3ec58a1e2be4d6
andig <cpuidle@gmx.de>
2023-12-06 16:44:16 +0100

    Add test for AutomaticEnv

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -3,9 +3,16 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"os"
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/fatih/structs"
+	"github.com/jeremywohl/flatten"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/maps"
 )
 
 func TestUnwrap(t *testing.T) {
@@ -36,4 +43,48 @@ func TestRedact(t *testing.T) {
 	if res := redact(secret); strings.Contains(res, "geheim") || !strings.Contains(res, "public") {
 		t.Errorf("secret exposed: %v", res)
 	}
+}
+
+func TestEnv(t *testing.T) {
+	v := viper.New()
+
+	// initConfig()
+	v.SetEnvPrefix("evcc")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	// register all known config keys
+	flat, _ := flatten.Flatten(structs.Map(conf), "", flatten.DotStyle)
+	for _, k := range maps.Keys(flat) {
+		t.Log(k)
+		_ = v.BindEnv(k)
+	}
+
+	os.Setenv("EVCC_DATABASE_DSN", "dsn")
+	os.Setenv("EVCC_SITE_TITLE", "foo")
+	// os.Setenv("EVCC_LOADPOINTS_0_TITLE", "baz")
+
+	yaml := `
+database:
+  type: sqlite
+  dsn: bar
+`
+
+	v.SetConfigType("yaml")
+	assert.NoError(t, v.ReadConfig(strings.NewReader(yaml)))
+
+	var conf globalConfig
+	assert.NoError(t, v.UnmarshalExact(&conf))
+
+	// predeclared struct
+	assert.Equal(t, "sqlite", conf.Database.Type)
+	assert.Equal(t, "dsn", conf.Database.Dsn)
+
+	// map[string]interface{}
+	assert.Equal(t, "foo", conf.Site["title"])
+
+	// []map[string]interface{}
+	// if assert.Len(t, conf.Loadpoints, 1) {
+	// 	assert.Equal(t, "baz", conf.Loadpoints[0]["title"])
+	// }
 }
